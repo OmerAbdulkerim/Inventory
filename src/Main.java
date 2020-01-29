e5c85cbe415eabead44854b9ad200a7e9ca3795f
Omer Abdulkerim Mehanovic <oamehanovic@gmail.com>
2020-01-29 16:30:16 +0100

    added increase quantity method

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,20 @@ public class Main {
     }
 
     static void menu(){
-        System.out.println("1) Create object: " + "\n2) Update item: " + "\n3) Remove item: "
+        System.out.println("0) Create object: " + "\n1) Add item" + "\n2) Update item: " + "\n3) Remove item: "
         + "\n4) List all items: ");
 
         Scanner input = new Scanner((System.in));
 
         switch (input){
-            case 1:
+            case 0:
                 ItemManager.createItem();
+                break;
+            case 1:
+                System.out.println("Insert object id");
+                int id = input.nextInt();
+                ItemManager.increaseQuantity(id);
+                menu();
                 break;
             case 2:
                 ItemManager.updateItem();
